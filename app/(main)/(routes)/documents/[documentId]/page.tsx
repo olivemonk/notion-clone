17a906d210dbecc9a2832d1d980017788f6f0b70
OliveMonk <gp7.official@gmail.com>
2023-12-23 23:53:57 +0530

    added edge store and cover image

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,45 +1,51 @@
-'use client';
+"use client";
 
 import Cover from "@/components/cover";
 import ToolBar from "@/components/toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 
 interface DocumentIdPageProps {
-    params: {
-        documentId: Id<'documents'>
-    }
+  params: {
+    documentId: Id<"documents">;
+  };
 }
 
-const DocumentIdPage = ({params}:DocumentIdPageProps) => {
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
 
-    const document = useQuery(api.documents.getById, {
-        documentId: params.documentId
-    })
-
-    if(document === undefined){
-        return (<div>
-            loading
-        </div>)
-    }
-
-    if(document === null){
-        return (
-            <div>
-                Not found
-            </div>
-        )
-    }
-
-    return ( 
-        <div className="pb-40">
-            ,<Cover url={document.coverImage}/>
-            <div className="md:max-3xl lg:max-w-4xl mx-auto">
-                <ToolBar initialData={document} />
-            </div>
+  if (document === undefined) {
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pt-4 pl-8 ">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
         </div>
-     );
-}
- 
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      ,<Cover url={document.coverImage} />
+      <div className="md:max-3xl lg:max-w-4xl mx-auto">
+        <ToolBar initialData={document} />
+      </div>
+    </div>
+  );
+};
+
 export default DocumentIdPage;
--- components/cover.tsx
+++ components/cover.tsx
@@ -10,6 +10,7 @@ import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
 import { useEdgeStore } from "@/lib/edgestore";
+import { Skeleton } from "./ui/skeleton";
 
 interface CoverProps {
   url?: string;
@@ -67,5 +68,12 @@ const Cover = ({ url, preview }: CoverProps) => {
     </div>
   );
 };
+
+
+Cover.Skeleton = function CoverSkeleton() {
+  return (
+    <Skeleton className="w-full h-[12vh]"/>
+  )
+}
 
 export default Cover;
