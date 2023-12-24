f61b55084aebcd39e9fa6d9eec6ff6719c39993e
OliveMonk <gp7.official@gmail.com>
2023-12-24 14:48:19 +0530

    minor bug fixes

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -4,7 +4,7 @@ import Heroes from "./_components/heroes";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-full flex flex-col dark:bg-[#1f1f1f]">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />
