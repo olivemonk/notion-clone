d946f1e21f7f859e2b07f3a98687f85eac541d6a
OliveMonk <gp7.official@gmail.com>
2023-12-24 14:30:13 +0530

    added published page and publish functuinality

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -8,6 +8,7 @@ import { useParams } from "next/navigation";
 import Title from "./title";
 import Banner from "./banner";
 import Menu from "./menu";
+import Publish from "./publish";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -51,6 +52,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
             className="flex items-center
          gap-x-2"
           >
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
